5441c45dff3fe2f4bd76d4aa83e4805f4fe0d365
Michael Auchter <a@phire.org>
2022-12-12 18:40:46 +0000

    user

--- beetsplug/beetstream/users.py
+++ beetsplug/beetstream/users.py
@@ -6,9 +6,8 @@ import xml.etree.cElementTree as ET
 @app.route('/rest/getUser', methods=["GET", "POST"])
 @app.route('/rest/getUser.view', methods=["GET", "POST"])
 def user():
-    res_format = request.values.get('f') or 'xml'
-    if (is_json(res_format)):
-        return jsonpify(request, wrap_res("user", {
+    return subsonic_response(request, {
+        "user": {
             "username" : "admin",
             "email" : "foo@example.com",
             "scrobblingEnabled" : True,
@@ -26,27 +25,5 @@ def user():
             "videoConversionRole" : True,
             "avatarLastChanged" : "1970-01-01T00:00:00.000Z",
             "folder" : [ 0 ]
-        }))
-    else:
-        root = get_xml_root()
-        u = ET.SubElement(root, 'user')
-        u.set("username", "admin")
-        u.set("email", "foo@example.com")
-        u.set("scrobblingEnabled", "true")
-        u.set("adminRole", "true")
-        u.set("settingsRole", "true")
-        u.set("downloadRole", "true")
-        u.set("uploadRole", "true")
-        u.set("playlistRole", "true")
-        u.set("coverArtRole", "true")
-        u.set("commentRole", "true")
-        u.set("podcastRole", "true")
-        u.set("streamRole", "true")
-        u.set("jukeboxRole", "true")
-        u.set("shareRole", "true")
-        u.set("videoConversionRole", "true")
-        u.set("avatarLastChanged", "1970-01-01T00:00:00.000Z")
-        f = ET.SubElement(u, 'folder')
-        f.text = "0"
-
-        return Response(xml_to_string(root), mimetype='text/xml')
+        }
+    })
